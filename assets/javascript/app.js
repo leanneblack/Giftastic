993ec1faceedd99fb5de34eac212bc8a887d3b48
leanneblack <leanne.black@cox.net>
2019-03-02 14:45:33 -0800

    basic functioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,15 @@
-// create array for wild animals in variable topics
+// create array for teams in variable topics
 
-var topic =[
-    "Sharks", "Parrots", "Tigers", "Wolves", "Sea Otters"];
+var top5 =[
+    "Liverpool", "", "Man City", "Tottenham", "Arsenal", "Man United"];
 
     $("button").on("click", function() {
 
-        var shark = $(this).attr("data-shark");
+        $("#teamGif").empty();
+        var teams = $(this).attr("data-name");
 
-        // Constructing a queryURL using the animal name
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "shark"  + "&api_key=ETmkYzIbqrIvp6FYgWeTjBM0gF1ypKkR&limit=10";
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + teams + "&api_key=ETmkYzIbqrIvp6FYgWeTjBM0gF1ypKkR&limit=10";
   
         // Performing an AJAX request with the queryURL
         $.ajax({
@@ -17,33 +18,52 @@ var topic =[
         })
           // After data comes back from the request
           .then(function(response) {
-            console.log(queryURL);
-  
-            console.log(response);
-            // storing the data from the AJAX request in the results variable
+           
             var results = response.data;
   
             // Looping through each result item
             for (var i = 0; i < results.length; i++) {
   
               // Creating and storing a div tag
-              var animalDiv = $("<div>");
+              var teamDiv = $("<div>");
+              teamDiv.addClass("images");
   
               // Creating a paragraph tag with the result item's rating
-              var p = $("<p>").text("Rating: " + results[i].rating);
+              var p = $("<p>").html("Rating: " + results[i].rating);
   
               // Creating and storing an image tag
-              var animalImage = $("<img>");
-              // Setting the src attribute of the image to a property pulled off the result item
-              animalImage.attr("src", results[i].images.fixed_height.url);
+              var teamImage = $("<img>");
+
+              // Setting the src attribute of the image to a property pulled off the result item and creating still and animate attribute for gifs.
+            teamImage.attr("src", results[i].images.fixed_height_small.url);
+
+            teamImage.attr("data-still",results[i].images.fixed_height_small_still.url);
+            teamImage.attr("data-animate", results[i].images.fixed_height_small.url);
+            teamImage.attr("data-state", "still");
+
+
+              // Appending the paragraph and image tag 
+              teamDiv.append(p);
+              teamDiv.append(teamImage);
   
-              // Appending the paragraph and image tag to the animalDiv
-              animalDiv.append(p);
-              animalDiv.append(animalImage);
-  
-              // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-              $("#sharkGif").prepend(animalDiv);
+              
+             $("#teamGif").prepend(teamDiv);
             }
           });
-      });
+
+
+        //How to get to work so theres no need to load more images as in the example???
+
+        $(".gif").on("click", function() {
+        var state = $(this).attr("data-state");
+            if (state === "still") {
+                teamImage.attr("src", $(this).attr("data-animate"));
+               teamImage.attr("data-state", "animate");
+              } else {
+                teamImage.attr("src", $(this).attr("data-still"));
+                teamImage.attr("data-state", "still");
+              }
+            });
+
+        });
    
